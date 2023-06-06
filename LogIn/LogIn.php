50879ca21be0aa6c5c323b1d34a7f5947151e1c8
KuramaU <ivannikovamargaryta@gmail.com>
2023-06-07 02:19:42 +0300

    Add a registration and login forms

---
+++ LogIn/LogIn.php
@@ -0,0 +1,24 @@
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="/css/LogIn.css">
+    <title>LogIn</title>
+</head>
+<body>
+<div class="login-form">
+    <h1>LogIn</h1>
+    <form action="#" method="post">
+        <img src="/images/cat.png" alt="Avatar" class="avatar">
+        <p>Email: </p>
+        <input type="text" name="email" placeholder="Email">
+        <p>Password: </p>
+        <input type="text" name="password" placeholder="Password">
+        <button type="submit">Login</button>
+    </form>
+</div>
+</body>
+</html>
---
+++ Register/register.php
@@ -0,0 +1,31 @@
+<<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration</title>
+    <link rel="stylesheet" type="text/css" href="/css/register.css">
+
+</head>
+<body>
+<div class="registration-form">
+    <h1>Registration</h1>
+    <form action="#" method="post">
+        <img src="/images/cat.png" alt="Avatar" class="avatar">
+        <p>Name:</p>
+        <input type="text" name="name" placeholder="Name">
+        <p>Surname: </p>
+        <input type="text" name="surname" placeholder="Surname">
+        <p>Phone: </p>
+        <input type="text" name="phone" placeholder="Phone">
+        <p>Email: </p>
+        <input type="text" name="email" placeholder="Email">
+        <p>Password: </p>
+        <input type="text" name="password" placeholder="Password">
+        <button type="submit">Registration</button>
+    </form>
+</div>
+</body>
+</html>
